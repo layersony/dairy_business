7252e04dabd2483fe0be0088fe3e0e823e42e54b
layersony <sammaingi5@gmail.com>
2021-03-13 02:21:44 +0300

    created backend part of first part of report generation

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var number = document.getElementById("nos_sheds").value ;
+// var number = document.getElementById("nos_sheds").value; //user input
 
-function addshed(numb){
-  var container = document.getElementById("newslots");
+function addshed(){
+  var number = document.getElementById("nos_sheds").value;
+  var container = document.getElementById("newslots"); //this is div
+
   while(container.hasChildNodes()){
     container.removeChild(container.lastChild);
   }
@@ -11,17 +13,32 @@ function addshed(numb){
     var input = document.createElement("input");
     input.type="text";
     input.name="shed " + i;
+    input.id= "num"+i
     container.appendChild(input);
     container.appendChild(document.createElement("br"));
   }
+  var genttl = document.createElement("input");
+  genttl.type="button";
+  genttl.id= "genB";
+  genttl.value = "Generate Total";
+  genttl.onclick = function(){totalMilk(number)};
+  container.appendChild(genttl);
 };
 
 function sumShed(num){
   sum = 0
   for (var i = 0; i < num.length; i++){
-    sum += num[i];
+    sum += parseInt(num[i]);
   }
-
+  console.log(sum)
   return sum;
 };
 
+function totalMilk(number){
+  var totalMilk = [];
+  total = 0;
+  for (i=0; i<number; i++){
+    totalMilk.push(document.getElementById("num"+i).value);
+  };
+  return sumShed(totalMilk);
+};
